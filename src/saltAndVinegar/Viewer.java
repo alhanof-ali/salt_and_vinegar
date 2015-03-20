05c96b2cbbbd721bc0e3291650985b0671ea078d
Josh <j.wise1995@me.com>
2015-03-20 15:45:07 +1000

    Added first class

---
+++ src/saltAndVinegar/Viewer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package saltAndVinegar;
+
+/**
+ * @author Josh
+ *
+ */
+public class Viewer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
